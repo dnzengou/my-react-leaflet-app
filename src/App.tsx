9ffd76f43a996746eb76d20e38a54420c2168d54
dnzengou <desire.yavro@gmail.com>
2023-06-11 03:28:14 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
+import portfolioData from "./data/portfolioData.json"
+import "leaflet/dist/leaflet.css";
+import './App.css';
+
+import L from "leaflet";
+
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
+});
+
+function App() {
+    
+  return (
+   
+    <MapContainer center={[4.389955, 18.548918]} zoom={3} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+          
+          {portfolioData.map(portfolio => (
+              < Marker
+                  key={portfolio.Topic_ID}
+                  position={[
+                      portfolio.Location.Latitude,
+                      portfolio.Location.Longitude
+                  ]}
+               />          
+          ))}
+          
+      <Marker position={[4.389955, 18.548918]}>
+        <Popup>
+          It starts <br /> here!
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default App;
